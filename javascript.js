635cd8636402085bae86b1513973a79684f35883
Elwood Crocker <elwood.crocker@gmail.com>
2023-08-30 10:50:11 -0230

    Tidy functions

--- javascript.js
+++ javascript.js
@@ -2,21 +2,29 @@
 
 const outputDiv = document.querySelector('.output-div');
 let slider = document.getElementById('myRange');
-let colorInput = document.getElementById("colorpicker").value;
+let color = document.getElementById("colorpicker").value;
 
 
 document.onload = createDiv(slider.value)
-outputDiv.addEventListener('mouseover',changeColor, false)
+outputDiv.addEventListener('mouseover',changeColor(color), false)
 
 document.getElementById('colorpicker').onchange = function (){
-    colorInput = this.value;
+    changeColor(this.value);
 }
 document.getElementById('clear').onclick = function (){
     createDiv(slider.value);
 }
 document.getElementById('myRange').oninput = function(){
     createDiv(this.value)
-}    
+}
+document.getElementById('rainbow').onclick = function(){
+    changeColor(randomColor());
+                
+}
+
+
+
+
     
 
 
@@ -42,12 +50,27 @@ function createDiv(input){
 }
    
 
-function changeColor(){
+function changeColor(colorInput){
+
+
     const pixel = document.querySelectorAll('.output-div > div').forEach(pixel => {
         pixel.addEventListener('mouseover',() => {
-            pixel.style.background = `${colorInput}`;
+            pixel.style.background = colorInput;
         })
+        
     })
+}
+
+
+function randomColor(){
+    let R = Math.floor(Math.random()*256)
+    let G = Math.floor(Math.random()*256)
+    let B = Math.floor(Math.random()*256)
+
+    let color = `rgb(${R},${G},${B})`;
+
+    return color;
+
 }
 
 
